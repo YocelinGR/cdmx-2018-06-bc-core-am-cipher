a68ffe341440ea9a366b36d1635e9d83fa828489
YocelinGRomero <garcia_romero.y@hotmail.com>
2018-06-17 11:53:34 -0500

    Hacker Edition Terminadas

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,7 @@
 window.cipher = {
  //MÉTODO ENCODE: CODIFICA EL TEXTO
+ //objString1 : objString1,
+ //objString2 : objString2,
   encode : (string, offset) => {
   offset = parseInt(offset);
     //console.log(string, offset);
@@ -70,91 +72,21 @@ window.cipher = {
   //result.innerHTML = decodeToAlphabet.join("");
   return decodeToAlphabet.join("");
 },
-
-createCipherWithOffset : (offset) =>{
-  //console.log(offset);
-  /*let objectWithOffset ={
-
-    encode: let encodeOffset = cipher.encode(string, offset),
-    decode: let decodeOffset = cipher.decode(string, offset)
-  }
-  console.log(objectWithOffset);
-  return objectWithOffset;*/
-  return ("Codificación: " + cipher.encode("Hola", offset) + " ,Decodificación: " + cipher.decode("ovsh", offset));
-}
-};
-
-//window.cipher = {
-  // ...
-//};
-
-/*const abecedarioOriginal = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z'];
-const posicionesOriginales = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
-let resultado = document.getElementById("resultado");
-
-const ingresarClave = () => {
-  let clave = parseInt(document.getElementById("clave").value);
-  return clave;
-}
-const encuentraIndice = (str) =>{
-   //str = str.toUpperCase();
-   let posicionesStr = new Array(abecedarioOriginal.length);
-   for (let i=0; i<(str.length); i++){
-      for (let j =0; j<(abecedarioOriginal.length); j++){
-        if ((str[i]) === (abecedarioOriginal[j])){
-           posicionesStr[i]=j;
+   createCipherWithOffset : (offset) =>{
+      let encodePhrase = objString1.value;
+      let decodePhrase = objString2.value;
+      let objCipher ={
+        offset : offset,
+        encodePhrase : encodePhrase,
+        decodePhrase : decodePhrase,
+        encode : (encodePhrase) =>{
+          return cipher.encode(encodePhrase,offset);
+        },
+        decode : (decodePhrase) =>{
+          return cipher.decode(decodePhrase,offset);
         }
       }
+      return ("Frase cifrada: "+ objCipher.encode(encodePhrase) + "\n"+ "Frase Descifrada: " + objCipher.decode(decodePhrase));
+      //return objCipher;
    }
-   return posicionesStr;
-}
-
-const codificarPosiciones = (vector) => {
-  let codigo = ingresarClave();
-  let numPosiciones = vector.length;
-  let posicionesCodificadas = new Array(numPosiciones);
-  for (let i=0; i<(vector.length); i++){
-    posicionesCodificadas[i] = ((vector[i]+codigo) % 26);
-  }
-  return posicionesCodificadas;
-}
-const codificar = (posCodificadas) => {
-  let numCifrado = posCodificadas.length;
-  let vectorCifrado = new Array(numCifrado);
-  for (let i=0; i<numCifrado; i++){
-     for (let j =0; j<(posicionesOriginales.length); j++){
-       if ((posCodificadas[i]) === (posicionesOriginales[j])){
-          vectorCifrado[i] = abecedarioOriginal[j];
-       }
-     }
-  }
-  return vectorCifrado;
-}
-
-const decodificarPosiciones = (vectorPos) => {
-  let codigo1 =33;
-  let numPosiciones1 = vectorPos.length;
-  let posicionesDecodificadas = new Array(numPosiciones1);
-  for (let i=0; i<numPosiciones1; i++){
-    posicionesDecodificadas[i] = (26 + ((vectorPos[i]-codigo1)%26))%26;
-  }
-  return posicionesDecodificadas;
-}
-
-const encoderCesar = () => {
-  let vectorACodificar = (document.getElementById("aCifrar").value).toUpperCase();
-  let pos = encuentraIndice(vectorACodificar);
-  let indiceCodificado = codificarPosiciones(pos);
-  let vectorCodificado = codificar(indiceCodificado);
-  //return console.log(vectorCodificado);
-  return resultado.innerHTML = "El resultado es: "+ vectorCodificado.join("");
-}
-const decoderCesar = () => {
-  let vectorADecodificar = (document.getElementById("aCifrar").value).toUpperCase();
-  let posDecode =encuentraIndice(vectorADecodificar);
-  let indicesDeCodificado = decodificarPosiciones(posDecode);
-  let decodificado = codificar(indicesDeCodificado);
-  //return console.log(decodificado);
-  return resultado.innerHTML = "El resultado es: "+ decodificado.join("");
-}
-*/
+};
--- src/index.js
+++ src/index.js
@@ -6,22 +6,28 @@ let encode = document.getElementById("encode");
 let decode = document.getElementById("decode");
 let result = document.getElementById("result");
 let withOffset = document.getElementById("hackerEdition");
-let objString = document.getElementById("objString");
+let objString1 = document.getElementById("objString1");
+let objString2= document.getElementById("objString2");
 let hackerEdition = document.getElementById("wOffset");
 // DEFINIENDO EVENTOS DEL DOM CON addEventListener
 
+//Evento que llama a mandar el botón de codificar
 encode.addEventListener("click", cipherFun);
 function cipherFun () {
   result.innerHTML = window.cipher.encode(string.value, offset.value);
 }
 
-
+//Evento que manda a llamr el botón de decodificar
 decode.addEventListener("click", deCipherFun);
 function deCipherFun () {
   result.innerHTML = window.cipher.decode(string.value, offset.value);
 }
 
+//Evento que manda a llamar la funcion extra
 withOffset.addEventListener("click", withOffsetFun);
 function withOffsetFun () {
+  objString1.value;
+  objString1.value;
   result.innerHTML = window.cipher.createCipherWithOffset(offset.value);
+
 }
